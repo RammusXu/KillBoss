31d71c17f9151817ca363b238bca92e489f35a23
twho <tsungwei50521@gmail.com>
2014-09-25 17:01:11 +0800

    .

--- KillBoss/src/com/ntut/killboss/core/GameView.java
+++ KillBoss/src/com/ntut/killboss/core/GameView.java
@@ -245,15 +245,11 @@ public class GameView extends SurfaceView {
 				// TODO Auto-generated method stub
 				_hero.skillBshot();
 
-				ObjectSkill temp1 = new ObjectSkill3(getContext(),
+				ObjectSkill temp = new ObjectSkill3(getContext(),
 						GameView._objectSkillsHero, _hero.get_x(), _hero
 								.get_y(), _hero.get_direction());
-				ObjectSkill temp2 = new ObjectSkill3(getContext(),
-						GameView._objectSkillsHero, _hero.get_x(), _hero
-								.get_y() + _hero.get_height(), _hero
-								.get_direction());
-				GameView._objectSkillsHero.add(temp1);
-				GameView._objectSkillsHero.add(temp2);
+
+				GameView._objectSkillsHero.add(temp);
 			}
 		}, 530);
 		new Handler().postDelayed(new Runnable() {
